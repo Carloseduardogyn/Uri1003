ce8d755b550377ee80189e93630089a9e8af9fac
Carlos Eduardo <carloseduardogomeslima1@gmail.com>
2023-02-09 10:29:26 -0300

    Uri1003

---
+++ Uri1003.java
@@ -0,0 +1,25 @@
+package Uri;
+
+import java.util.Scanner;
+
+public class Uri1003 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int A;
+		int B;
+
+		A = sc.nextInt();
+		B = sc.nextInt();
+
+		int soma = A + B;
+
+		System.out.println("SOMA = " + soma);
+		
+		sc.close();
+
+	}
+
+}
